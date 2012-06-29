e546800e241b6fa24c97be4915e1af63efeb430f
Adrián Suárez <upadrian@gmail.com>
2012-06-29 15:29:20 -0300

    Initial Commit

---
+++ jHelpers-min.js
@@ -0,0 +1,11 @@
+/*
+ * jHelpers
+ * Colección de helpers para jQuery y javascript.
+ * Las funciones se acceden mediante Helpers.<function>() ... 
+ * Algunas se instancian vía prototype como ltrim para Strings
+ * Author: upadrian@gmail.com 2012
+ * Versión: 1.0 20120629
+ * 
+ * 
+ * */
+function Helpers(){if(String.prototype.ltrim==null)String.prototype.ltrim=function(a){return this.replace(new RegExp("^["+(a||"\\s")+"]+","g"),"")};if(String.prototype.rtrim==null)String.prototype.rtrim=function(a){return this.replace(new RegExp("["+(a||"\\s")+"]+$","g"),"")};if(String.prototype.trim==null)String.prototype.trim=function(a){return this.ltrim(a).rtrim(a)};this.guessFileTypeFromFileInput=function(a){if(typeof a=="undefined")return null;if(a instanceof jQuery)a=a.get(0);if(!jQuery.browser.msie)try{return a.files[0].type?a.files[0].type:null}catch(b){return null}else{try{var c=$(a).val().split(".");var d=c[c.length-1].toLowerCase();return d=="csv"||d=="txt"?"text/plain":null}catch(b){return null}}}}var Helpers=new Helpers
---
+++ jHelpers.js
@@ -0,0 +1,54 @@
+/*
+ * jHelpers
+ * Colección de helpers para jQuery y javascript.
+ * Las funciones se acceden mediante Helpers.<function>() ... 
+ * Algunas se instancian vía prototype como ltrim para Strings
+ * Author: upadrian@gmail.com 2012
+ * Versión: 1.0 20120629
+ * 
+ * 
+ * */
+function Helpers(){
+	//ltrim
+	if (String.prototype.ltrim == null) 
+		String.prototype.ltrim = function(chars) {
+			return this.replace(new RegExp("^[" + (chars || "\\s") + "]+", "g"), "");
+		}
+	//rtim
+	if (String.prototype.rtrim == null) 
+		String.prototype.rtrim = function(chars) {
+			return this.replace(new RegExp("[" + (chars || "\\s") + "]+$", "g"), "");
+		}
+	//trim
+	if (String.prototype.trim == null) 
+		String.prototype.trim = function(chars) {
+			return this.ltrim(chars).rtrim(chars);
+		}
+	//guessFileTypeFromFileInput
+    this.guessFileTypeFromFileInput = function(fileInput){
+		if(typeof(fileInput)=="undefined") return null;
+		//es fileInput un elemento jquery o es un elemento del DOM? si es jQuery, traemos el elemento dom
+		if(fileInput instanceof jQuery)
+			fileInput = fileInput.get(0);
+		//si no es internet explorer, vamos por la mejor:
+		if(!jQuery.browser.msie)
+			try{
+				return (fileInput.files[0].type)?fileInput.files[0].type:null;
+			}catch(e){
+				return null;		
+			}
+		else {
+			//la complicada
+			try{
+				var parts = $(fileInput).val().split(".");
+				var ext = parts[parts.length-1].toLowerCase();
+				return (ext == "csv" || ext == "txt")?"text/plain":null;
+			}catch(e){
+				return null;
+			}
+		}        
+	}
+		
+}
+
+var Helpers = new Helpers();
